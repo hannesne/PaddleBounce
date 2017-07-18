2a32e833ea369838934b26c87980c73a3af4adfa
Hannes Nel <hannes.nel@microsoft.com>
2017-07-18 16:32:08 +1200

    Basic app start point with ball and blade.
    Ball will get gravity and bounce on the disk when you tap on it.

---
+++ Assets/PaddleBounce/BallBehavour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class BallBehavour : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Rigidbody ballRigidBody = this.gameObject.GetComponent<Rigidbody>();
+        ballRigidBody.useGravity = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
