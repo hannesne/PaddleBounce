5197f06c82d094d9c44b256192b9dd4e7992bbcb
Hannes Nel <hannes.nel@microsoft.com>
2017-07-19 17:03:56 +1200

    Draggable paddle, reset ball, bounce reliability update
    Made the paddle draggable. Ball will now reset when drag stops. Also
    changed timing, physics settings and rigidbody settings on the ball and
    paddle to make bouncing more reliable, and prevent the ball from falling
    through the paddle.

--- Assets/PaddleBounce/BallBehavour.cs
+++ Assets/PaddleBounce/BallBehavour.cs
@@ -3,21 +3,8 @@ using System.Collections;
 using HoloToolkit.Unity.InputModule;
 using System;
 
-public class BallBehavour : MonoBehaviour, IInputClickHandler
+public class BallBehavour : MonoBehaviour
 {
-    public GameObject Cursor;
-
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        //give the ball gravity so it starts falling.
-        Rigidbody ballRigidBody = this.gameObject.GetComponent<Rigidbody>();
-        ballRigidBody.useGravity = true;
-        //Hide the cursor.
-        if (this.Cursor != null)
-        {
-            this.Cursor.SetActive(false);
-        }
-    }
 
     // Use this for initialization
     void Start()
--- Assets/PaddleBounce/BladeBehaviour.cs
+++ Assets/PaddleBounce/BladeBehaviour.cs
@@ -1,13 +1,55 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class BladeBehaviour : MonoBehaviour {
+public class BladeBehaviour : MonoBehaviour, IInputHandler
+{
+    public GameObject Cursor;
+    public GameObject Ball;
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        Transform ballTransform = Ball.GetComponent<Transform>();
+        originalPosition = ballTransform.position;
+        originalRotation = ballTransform.rotation;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
+    
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        SetGameActive(true);
+    }
+
+    private void SetGameActive(bool active)
+    {
+        Rigidbody ballRigidBody = Ball.GetComponent<Rigidbody>();
+        ballRigidBody.useGravity = active;
+        if (!active)
+        {
+            ballRigidBody.isKinematic = true;
+            ballRigidBody.MovePosition(originalPosition);
+            ballRigidBody.MoveRotation(originalRotation);
+            ballRigidBody.isKinematic = false;
+        }
+        //Hide the cursor.
+        if (this.Cursor != null)
+        {
+            this.Cursor.SetActive(!active);
+        }
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        SetGameActive(false);
+        Ball.GetComponent<Transform>().SetPositionAndRotation(originalPosition, originalRotation);
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
