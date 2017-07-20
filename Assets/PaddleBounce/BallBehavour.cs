f90ff8aee06bd1b3e5dc507ef9d921b49650a78d
Hannes Nel <hannes.nel@microsoft.com>
2017-07-20 16:42:17 +1200

    Added bounce sound effect

--- Assets/PaddleBounce/BallBehavour.cs
+++ Assets/PaddleBounce/BallBehavour.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 using HoloToolkit.Unity.InputModule;
 using System;
+using HoloToolkit.Unity;
 
 public class BallBehavour : MonoBehaviour
 {
 
+    AudioSource audioSource;
+
     // Use this for initialization
     void Start()
     {
-
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        //preload our audio data
+        audioSource.clip.LoadAudioData();
     }
 
     // Update is called once per frame
@@ -17,4 +22,13 @@ public class BallBehavour : MonoBehaviour
     {
 
     }
+
+    // OnCollisionExit is called when this collider/rigidbody has stopped touching another rigidbody/collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+        
+    }
+
+
 }
