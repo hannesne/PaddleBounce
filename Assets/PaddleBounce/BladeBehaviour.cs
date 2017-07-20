4b3bcb2c223339acee989e733748e4e59a9e2721
Hannes Nel <hannes.nel@microsoft.com>
2017-07-20 19:45:29 +1200

    Found alternative fix for ball getting stuck in the air problem.
    We just need to wake it up manually when it leaves the collision with
    the paddle.

--- Assets/PaddleBounce/BladeBehaviour.cs
+++ Assets/PaddleBounce/BladeBehaviour.cs
@@ -9,8 +9,6 @@ public class BladeBehaviour : MonoBehaviour, IInputHandler
     public GameObject Ball;
     private Vector3 originalPosition;
     private Quaternion originalRotation;
-    private bool checkMoving;
-    private bool gameActive;
 
     // Use this for initialization
     void Start ()
@@ -18,7 +16,6 @@ public class BladeBehaviour : MonoBehaviour, IInputHandler
         Transform ballTransform = Ball.transform;
         originalPosition = ballTransform.position;
         originalRotation = ballTransform.rotation;
-
     }
     
 
@@ -29,17 +26,15 @@ public class BladeBehaviour : MonoBehaviour, IInputHandler
 
     private void SetGameActive(bool active)
     {
-        gameActive = active;
         Rigidbody ballRigidBody = Ball.GetComponent<Rigidbody>();
         ballRigidBody.useGravity = active;
         if (!active)
         {
             ballRigidBody.isKinematic = true;
-            ballRigidBody.MovePosition(originalPosition);
-            ballRigidBody.MoveRotation(originalRotation);
+            Ball.GetComponent<Transform>().SetPositionAndRotation(originalPosition, originalRotation);
             ballRigidBody.isKinematic = false;
         }
-        //Hide the cursor.
+        //Hide or show the cursor.
         if (this.Cursor != null)
         {
             this.Cursor.SetActive(!active);
@@ -49,44 +44,13 @@ public class BladeBehaviour : MonoBehaviour, IInputHandler
     public void OnInputUp(InputEventData eventData)
     {
         SetGameActive(false);
-        Ball.GetComponent<Transform>().SetPositionAndRotation(originalPosition, originalRotation);
-    }
-
-    List<float> lastBallVelocities = new List<float>();
-	
-	// Update is called once per frame
-	void Update () {
-        /*
-         *the ball sometimes gets stuck in thin air when you move the paddle away.
-         *we'll check if the game is going and the ball is not currently resting on the paddle
-         *if the velocity of the ball is static over the last 3 frames, we apply a small upwards force
-         *to get it going again, if we're sure it's moving, we just stop checking*/
-		if (gameActive && checkMoving)
-        {
-            float sqrMagnitude = Ball.transform.position.sqrMagnitude;
-            lastBallVelocities.Add(sqrMagnitude);
-
-            if (lastBallVelocities.Count == 3)
-            {
-                if (lastBallVelocities.All(item => item == sqrMagnitude))
-                {
-                    Debug.Log("Stuck? Pop!");
-                    Rigidbody ballRigidBody = Ball.GetComponent<Rigidbody>();
-                    ballRigidBody.AddForce(new Vector3(0, 0.0001f, 0), ForceMode.Impulse);
-                }
-                checkMoving = false;
-                lastBallVelocities.Clear();
-            }
-        }
-	}
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        checkMoving = false;
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        checkMoving = true;
+        //Sometimes when the ball comes to rest on the paddle, and you pull the paddle down from under it
+        //the rigid body isn't woken up and the ball stays stuck in mid-air.
+        //We make sure to wake the ball up when it leaves the paddle. 
+        Ball.GetComponent<Rigidbody>().WakeUp();
     }
 }
